d09c2ea145e0fbbfaebc8a59aa8c70131c03b70f
pavel_n <airboxnick@gmail.com>
2021-08-12 00:49:16 +0300

    Module 4 completed

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  let list = document.createElement('ul');
+  for(let elem of friends) {
+    list.insertAdjacentHTML('beforeend', `<li>${elem.firstName} ${elem.lastName}</li>`);
+  }
+
+  return list;
 }
