4d76d0e22e9cdd59bba60b877f1ec1b37b74ecc3
pavel_n <airboxnick@gmail.com>
2021-08-15 15:31:32 +0300

    Module 5 completed

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,12 @@
 function toggleText() {
-  // ваш код...
+  let button = document.querySelector('.toggle-text-button'),
+    text = document.querySelector('#text');
+
+  button.addEventListener('click', () => {
+    if (text.hidden === true) {
+      text.hidden = false;
+    } else {
+      text.hidden = true;
+    }
+  });
 }
--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,30 @@
 function initCarousel() {
-  // ваш код...
+  let carousel = document.querySelector('.carousel'),
+    buttonPrev = carousel.querySelector('.carousel__arrow_left'),
+    buttonNext = carousel.querySelector('.carousel__arrow_right'),
+    carouselTrack = carousel.querySelector('.carousel__inner'),
+    countSlides = carousel.querySelectorAll('.carousel__slide').length,
+    widthSlide = carousel.querySelectorAll('.carousel__slide')[0].clientWidth,
+    currentPosition = 1;
+    buttonPrev.style.display = 'none';
+
+    buttonPrev.addEventListener('click', () => {
+      if ( currentPosition > 0 ) {
+        carouselTrack.style.transform = `translateX(-${ (widthSlide * --currentPosition) - widthSlide }px)`;
+        buttonNext.style.display = 'flex';
+      }
+      if ( currentPosition <= 1 ) {
+        buttonPrev.style.display = 'none';
+      }
+    });
+
+    buttonNext.addEventListener('click', () => {
+      if ( currentPosition < countSlides ) {
+        carouselTrack.style.transform = `translateX(-${widthSlide * currentPosition++}px)`;
+        buttonPrev.style.display = 'flex';
+      }
+      if ( currentPosition >= countSlides ) {
+        buttonNext.style.display = 'none';
+      }
+    });
 }
